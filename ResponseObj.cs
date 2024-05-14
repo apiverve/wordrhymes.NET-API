ae34d27292dc99a8b2e7893774177e7bb0f43a10
APIVerve Team <hello@apiverve.com>
2024-05-14 15:46:15 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,45 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class rhymes
+{
+    [JsonProperty("score")]
+    public int score { get; set; }
+
+    [JsonProperty("pron")]
+    public string pron { get; set; }
+
+    [JsonProperty("word")]
+    public string word { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("word")]
+    public string word { get; set; }
+
+    [JsonProperty("rhymeCount")]
+    public int rhymeCount { get; set; }
+
+    [JsonProperty("rhymes")]
+    public rhymes[] rhymes { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
